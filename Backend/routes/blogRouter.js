a6689e9774868988ae808d1bc40d41edd4cf4742
MajdFoudeh <majdfoude@gmail.com>
2023-07-08 01:05:23 +0300

    blogs pages created

--- Backend/routes/blogRouter.js
+++ Backend/routes/blogRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const blogController = require("../controllers/blogController");
 
 router.get("/getAllBlogs", blogController.getAllBlogs);
-router.put("/deleteBlog/:id", blogController.deleteBlog);
+router.put("/deleteBlog", blogController.deleteBlog);
 router.put("/updateblog/:id", blogController.updateBlog);
 router.post("/addblog", blogController.addBlog);
 
--- Backend/server.js
+++ Backend/server.js
@@ -3,38 +3,37 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-
+const commentRouter = require("./routes/commentRouter");
+const blogRouter = require("./routes/blogRouter");
 
 // import routes
-const authRoute = require('./routes/userRouter');
-
-
-
-
+const authRoute = require("./routes/userRouter");
 
 // middlewear
-app.use( cors({
-  origin: ["http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "DELETE" , "PATCH"],
-  credentials: true,
-}));
-app.use(express.urlencoded({ extended:true}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 
-
 // use routes
 app.use(authRoute);
-
-
-
-
+app.use(blogRouter);
+app.use(commentRouter);
 
 module.exports = {
   server: app,
   start: () => {
     mongoose
-      .connect(process.env.DBURI, { useNewUrlParser: true, useUnifiedTopology: true })
+      .connect(process.env.DBURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
       .then(() => {
         app.listen(process.env.PORT, () => {
           console.log(`Starting server on port ${process.env.PORT}`);
